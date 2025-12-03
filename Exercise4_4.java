705713894d387f8de501691145bcba891b0e9f83
MuhammedAHMED7 <250541604@firat.edu.tr>
2025-12-03 22:09:55 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,8 @@
+/*
+ 250541604
+ Muhammed AHMED
+ 03.12.2025
+ */
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -10,8 +15,13 @@ public class Exercise4_4 {
 
     public static void main(String[] args) {
 
+        // 1) Donus degerini kullanmayin -> ne oluyor?
+        //Cevap1: Java'da bu hata vermez. Metot çalışır, işini yapar, dönen değer (return value) hafızada bir yere atanmadığı için kaybolur (ignore edilir).
         getNumber();
 
+        // 2) void metodu ifade icinde kullanin -> ne oluyor?
+        // System.out.println(sayHello() + 7);
+        //Cevap2: Java derleme hatası (compile-time error) verir. Çünkü void metotlar bir değer üretmez, dolayısıyla matematiksel veya mantıksal bir işleme sokulamazlar.
         
     }
 }
